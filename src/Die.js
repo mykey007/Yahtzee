eb4c5da24d4f01764db3e4a9c0cea61eeba1d528
Michael Mallowe <mmallowe@gmail.com>
2020-07-06 23:03:24 -0700

    calling handleclick without an arrow function

--- src/Die.js
+++ src/Die.js
@@ -2,12 +2,25 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+
+  handleClick() {
+    this.props.handleClick(this.props.idx)
+  }
   render() {
     return (
       <button
         className={"Die"}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={() => this.props.handleClick(this.props.idx)}
+        
+        // with an arrow function 
+        //onClick={() => this.props.handleClick(this.props.idx)}
+        // not using arrow function (premature performance)
+        onClick={this.handleClick}
       >
         {this.props.val}
       </button>
